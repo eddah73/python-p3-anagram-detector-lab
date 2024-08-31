cf3aa4f06fe46587bbd22f2096968e470bd14678
eddah73 <eddahsjr@gmail.com>
2024-08-31 20:05:35 +0300

    anargram word

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+    def match(self, list_word):
+        matches = []
+        for candidate in  list_word:
+            if len(candidate) == len(self.word) and ''.join(sorted(candidate)) == self.sorted_word:
+                matches.append(candidate)
+        return matches
